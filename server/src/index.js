b894c4406c9a75425fb82052a66944788cd571cb
ellapresso <ellapresso@gmail.com>
2019-04-02 19:45:09 +0900

    port 수정

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,16 @@
-const Koa = require("koa");
-const Router = require("koa-router");
+const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
 const router = new Router();
 
-router.get("/", (ctx, next) => {
-  ctx.body = "Koa";
-});
+// router.get("/", (ctx, next) => {
+//   ctx.body = "Koa";
+// });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen(process.env.PORT || 4000, () => {
-  console.log("MAD server is listening to port 4000");
+app.listen(process.env.PORT || 3000, () => {
+	console.log('MAD server is listening to port 3000');
 });
