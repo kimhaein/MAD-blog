e276aa7b0a312c971ea6e81f601f07387db4421b
haein <kendy93@sbcn.co.kr>
2019-04-09 18:32:27 +0900

    CodeView 컴포넌트 작업

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,7 +1,13 @@
 import { Col, Row, Avatar } from "antd";
+import moment from "moment";
 
-import Tags from "../../components/common/Tags";
-import Likes from "../../components/common/Likes";
+// components
+import Tags from "../common/Tags";
+import Likes from "../common/Likes";
+import Preview from "../common/CodeView/CodeView";
+
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
 
 import "./post.css";
 
@@ -27,10 +33,14 @@ const PostItem = ({ PostData }) => {
             </Avatar>
             <span>BY. {PostData.user}</span>
           </span>
-          <span className="postDate">{PostData.createAt}</span>
+          <span className="postDate">
+            {moment(PostData.createAt).format("MMM Do YY")}
+          </span>
         </div>
       </div>
-      <div className="postListBody">{11}</div>
+      <div className="postListBody">
+        <Preview markdown={PostData.body} />
+      </div>
       <div className="postListfooter">
         <Tags tagDatas={PostData.tag} />
         <Likes likeDatas={PostData.totalLikes} />
@@ -46,7 +56,8 @@ const Post = () => {
       user: "김혜인",
       createAt: "2019-01-03",
       updateAt: "2019-01-03",
-      body: "# 1234",
+      body:
+        "Hseded\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\n\n  *[Francesco Agnoletto](https://twitter.com/fragno92)*",
       tag: [
         "프론트개발",
         "자바스크립트",
@@ -70,7 +81,8 @@ const Post = () => {
       user: "김철수",
       createAt: "2019-01-01",
       updateAt: "2019-01-01",
-      body: "body",
+      body:
+        "> 안녕하세요 \n ```javascript \n const a = 1;\n console.log(a); // 1 \n```",
       tag: ["프론트개발", "자바스크립트", "리액트"],
       totalLikes: 30
     }
