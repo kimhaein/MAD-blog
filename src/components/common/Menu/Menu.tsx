2d42400754bc9f83ac4353ff2ea21f5f22a2d080
haein <kendy93@sbcn.co.kr>
2020-01-30 11:00:19 +0900

    깃허브 링크 추가

--- src/components/common/Menu/Menu.tsx
+++ src/components/common/Menu/Menu.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
-import { Drawer, Avatar } from "antd";
-import "./menu.css";
+import Link from 'next/link';
+import { Drawer, Avatar } from 'antd';
+import './menu.css';
 
 interface Props {
   isLogin: boolean;
@@ -12,47 +12,53 @@ interface Props {
 
 const Menu: React.FC<Props> = props => {
   return (
-    <Drawer placement={"left"} closable={true} onClose={props.onMenu} visible={props.isMenu}>
+    <Drawer placement={'left'} closable={true} onClose={props.onMenu} visible={props.isMenu}>
       {props.userName ? (
-        <div className="userInfo">
-          <Avatar src={props.userImg} size={150} style={{ backgroundColor: "#000" }}>
+        <div className='userInfo'>
+          <Avatar src={props.userImg} size={150} style={{ backgroundColor: '#000' }}>
             {props.userName.substring(0, 1).toUpperCase()}
           </Avatar>
           <p>{props.userName}</p>
         </div>
       ) : (
-        <div className="userInfo">
-          <Avatar size={150} style={{ backgroundColor: "#000" }}>
+        <div className='userInfo'>
+          <Avatar size={150} style={{ backgroundColor: '#000' }}>
             MAD
           </Avatar>
           <p>MAD BLOG</p>
         </div>
       )}
-      <ul className="menuList">
+      <ul className='menuList'>
         <li>
-          <Link href="/trend">
+          <Link href='/trend'>
             <span>트렌딩 차트</span>
           </Link>
         </li>
         {props.isLogin ? (
           <li>
-            <Link href="/mypage">
+            <Link href='/mypage'>
               <span>마이페이지</span>
             </Link>
           </li>
         ) : null}
       </ul>
-      <div className="menuFooter">
+      <div className='menuFooter'>
         {/* <div className="catImg" />
         <Link href="/readme">
           <h3>프로젝트 소개</h3>
         </Link> */}
-        <div className="gitHub">
+        <div className='gitHub'>
           <p>GitHub</p>
           <ul>
-            <li>황윤지 /</li>
-            <li> 김혜인 /</li>
-            <li>유윤선</li>
+            <li>
+              <Link href='https://github.com/ellapresso'>황윤지 /</Link>
+            </li>
+            <li>
+              <Link href='https://github.com/kimhaein'>김혜인 /</Link>
+            </li>
+            <li>
+              <Link href='https://github.com/yusop795'>유윤선</Link>
+            </li>
           </ul>
         </div>
       </div>
